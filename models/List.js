ca8bbb0b247438e981675ed8ddfd1322b1bb74d2
cmseibel <cam.m.seib@gmail.com>
2022-02-14 20:44:52 -0800

    Write List model.

---
+++ models/List.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class List extends Model{}
+
+List.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        media: {
+            type: DataTypes.ARRAY(DataTypes.INTEGER),
+            references: {
+                model: "media",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "list"
+    }
+)
+
+module.exports = List;
