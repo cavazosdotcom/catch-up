2596c277dcb8c89da23aa0f48d8369e161f914b9
cmseibel <cam.m.seib@gmail.com>
2022-02-14 21:08:18 -0800

    Write index.js as a one-stop-shop for Model exports.

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const List = require("./List");
 const Media = require("./Media");
 
 // Define table relationships
-List.hasMany(Media);
+List.belongsTo(User);
+List.hasMany(Media); // May not be correct? Since List just has an array of media(id)
 
 module.exports = {User, List, Media}; 
