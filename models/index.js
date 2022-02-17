8aff2e9dfbf6e0d86f162dd262bb99a341b8aa31
cmseibel <cam.m.seib@gmail.com>
2022-02-16 19:05:01 -0800

    Let Sequelize handle relationships cx

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const List = require("./List");
 const Media = require("./Media");
 
 // Define table relationships
-List.belongsTo(User);
-List.hasMany(Media); // May not be correct? Since List just has an array of media(id)
+// List.belongsTo(User);
+Media.belongsToMany(List);
 
 module.exports = {User, List, Media}; 
