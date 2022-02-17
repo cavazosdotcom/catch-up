03adcdfb35510043ef1e8db8149139abf3cf8841
cavazosdotcom <cavazosdotcom@gmail.com>
2022-02-16 20:26:22 -0800

    Added relationships to models

--- models/index.js
+++ models/index.js
@@ -3,7 +3,16 @@ const List = require("./List");
 const Media = require("./Media");
 
 // Define table relationships
-// List.belongsTo(User);
-Media.belongsToMany(List);
+
+List.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+List.hasMany(Media, {
+    foreignKey: 'media_id',
+    onDelete: 'Cascade'
+})
+
+// Media.belongsToMany(List);
 
 module.exports = {User, List, Media}; 
