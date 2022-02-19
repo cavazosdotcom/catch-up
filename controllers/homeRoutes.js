01e758a6d99b54596b86ed3d69eae470561ad7e2
cmseibel <cam.m.seib@gmail.com>
2022-02-19 13:54:17 -0800

    Write some stuff for home routes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,34 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const {List, Media, MediaList} = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
-    const list = require("../seeds/mediaData.json");
-    res.render("list", {logged_in: req.session.logged_in, list});
+
+    try{
+        const listData = await List.findAll({
+            where:{
+                user_id: req.session.user_id
+            },
+            include: [{ model: Media, through: MediaList }],
+        });
+        const list = await listData.toJSON();
+        res.json(list)
+        console.log(list);
+        console.log("works!!!!!")
+        // res.render("list", {logged_in: req.session.logged_in, list});
+    }catch(error){
+
+    }
 });
 
 router.get("/media", withAuth, async (req, res) => {
     res.render("media", {logged_in: req.session.logged_in});
-})
+});
+
+router.get("/media/:id", async (req, res) => {
+    const media_id = req.params.id;
+    res.render("media", {logged_in: req.session.logged_in, media_id})
+});
 
 router.get("/register", (req, res) => {
     if (req.session.logged_in) {
