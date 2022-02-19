670e303357c272a5bf3f13c4548ca31a17981cb1
cavazosdotcom <cavazosdotcom@gmail.com>
2022-02-19 10:24:28 -0800

    added basic working routes

--- controllers/api/mediaRoutes.js
+++ controllers/api/mediaRoutes.js
@@ -4,27 +4,91 @@ const { Media } = require('../../models');
 
 router.get('/', async (req, res) => {
     
-    try {
+  try {
 
-      const mediaData = await Media.findAll();
+    const mediaData = await Media.findAll();
+
+    res.status(200).json( mediaData );
+
+  } catch (err) {
+
+    res.status(400).json(err);
+
+  }
+});
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const mediaData = await Media.findByPk( req.params.id, {
+      // include: [{ model: Product }]
+    });
   
-      res.status(200).json( mediaData );
+    if ( !mediaData ) {
+      res.status(404).json({ message: 'No media found with this id!' });
+      return;
+    }
+
+    res.json( mediaData );
 
     } catch (err) {
-
-      res.status(400).json(err);
-
+      res.status(500).json(err);
     }
-  });
+});
 
 
+router.post('/', async (req, res) => {
+  try {
+    const newMedia = await Media.create( req.body );
+
+    res.status(200).json(newMedia);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
+router.put('/:id', async (req, res) => {
+  // update a media by its `id` value
+  try {
+    const mediaData = await Media.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    
+    if ( !mediaData ) {
+      res.status(404).json({ message: 'No media found with this id!' });
+      return;
+    }
+
+    res.status(200).json( mediaData );
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
+router.delete('/:id', async (req, res) => {
+  // delete a media by its `id` value
+  try {
+    const mediaData = await Media.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    if ( !mediaData ) {
+      res.status(404).json({ message: 'No media found with this id!' });
+      return;
+    }
 
-
+    res.status(200).json( mediaData );
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
