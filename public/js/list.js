4c754ef2b7799da5483baa3ee0663b6b513eb04f
cavazosdotcom <cavazosdotcom@gmail.com>
2022-02-24 00:46:43 -0800

    added some comments and deleted test lines of code

--- public/js/list.js
+++ public/js/list.js
@@ -19,18 +19,19 @@ const delButtonHandler = async (event) => {
 };
 
 const animate = async (event) => {
-
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
-
+    
+    // selects the specific row to animate
     const animate = document.querySelector(`.delete-animate${id}`);
     animate.classList.add('animate__animated', 'animate__bounceOutRight'); 
 
   } else {
     alert('failed to delete media')
-  }
-}
+  };
+};
 
+// Selects parent element to allow all of the rows to be a selectable for the animation
 document
   .querySelector('.delete-animate-parent')
   .addEventListener('click', animate );
