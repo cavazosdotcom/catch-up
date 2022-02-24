e880435ace394385bfa628e1df61ad25ea996d9d
cavazosdotcom <cavazosdotcom@gmail.com>
2022-02-24 00:40:43 -0800

    added animate.css animation for when media is deleted

--- public/js/list.js
+++ public/js/list.js
@@ -1,20 +1,42 @@
+// import 'animate.css';
+deleteBtn = document.querySelector('.delete-media');
+
 const delButtonHandler = async (event) => {
+    
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
       const response = await fetch(`/api/media/${id}`, {
         method: 'DELETE',
       });
-  
       if (response.ok) {
+        setTimeout(() => {
         document.location.replace('/');
+        }, 450);
       } else {
         alert('Failed to delete project');
       }
     }
 };
 
+const animate = async (event) => {
+
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const animate = document.querySelector(`.delete-animate${id}`);
+    animate.classList.add('animate__animated', 'animate__bounceOutRight'); 
+
+  } else {
+    alert('failed to delete media')
+  }
+}
 
 document
-    .querySelector('.delete-media')
-    .addEventListener('click', delButtonHandler);
+  .querySelector('.delete-animate-parent')
+  .addEventListener('click', animate );
+
+document
+  .querySelector('.delete-media')
+  .addEventListener('click', delButtonHandler );
+
