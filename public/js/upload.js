8246669f8f1cc5124fccca21c66286e07b37aea6
cmseibel <cam.m.seib@gmail.com>
2022-02-26 13:27:13 -0800

    Write demo image upload functionality.

---
+++ public/js/upload.js
@@ -0,0 +1,20 @@
+const registerFormHandler = async (event) => {
+    event.preventDefault();
+    const image = document.getElementById("image").files[0];
+    console.log(image);
+    const id = document.getElementById("id").value;
+    const data = new FormData();
+    data.append("image", image);
+    data.append("id", id);
+    const response = await fetch(`/api/media/image/`, {
+        method: "POST",
+        body: data,
+    });
+    if(response.ok){
+        alert("File upload successful.");
+    }
+};
+
+document
+    .getElementById("image-upload")
+    .addEventListener('submit', registerFormHandler);
